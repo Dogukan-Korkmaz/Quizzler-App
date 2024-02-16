aef41257ad9810953603af2790e224ea5950803b
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2024-02-16 21:26:24 +0300

    API Config.

--- ui.py
+++ ui.py
@@ -27,11 +27,11 @@ class QuizInterface():
         )
 
         true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_image, relief="flat")
+        self.true_button = Button(image=true_image, relief="flat", command=self.true_pressed)
         self.true_button.grid(row=4, column=0)
 
         false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_image, relief="flat")
+        self.false_button = Button(image=false_image, relief="flat", command=self.false_pressed)
         self.false_button.grid(row=4, column=3)
 
         self.get_next_question()
@@ -39,6 +39,26 @@ class QuizInterface():
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            self.canvas.config(bg="white")
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, func=self.get_next_question)
